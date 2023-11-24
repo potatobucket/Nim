eb5ad4352f43c7f5d377b4ba609f3ab54d3d0c26
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-11-24 15:46:28 -0600

    Player goes first works
    The game works if the player goes first. Is it elegant? Probably not as much as it could be.

--- game.py
+++ game.py
@@ -1,11 +1,55 @@
-tokens = 21
-validAnswers = ["first", "f", "last", "l"]
+import random as rnd
+from time import sleep as slp
 
-def first_or_second():
-    turn = None
+tokens = 21
+gameRun = True
+
+def first_or_second(turn = None):
+    validAnswers = ["first", "f", "last", "l"]
     while turn not in validAnswers:
         turn = input("Please choose whether you would like to go (F)IRST or (L)AST. ").lower()
     return turn
 
+def player_choose(tokenCount):
+    if tokens != None:
+        playerNumber = int(input(f"Tokens left: {tokenCount} | How many tokens do you want to take? (1-3) "))
+    while playerNumber < 1 or playerNumber > 3:
+        playerNumber = int(input(f"Tokens left: {tokenCount} | Please choose a number between 1 and 3. "))
+    return int(tokenCount - playerNumber)
+
+def cpu_choose(tokenCount):
+    if gameRun == True:
+        if tokenCount > 3 and tokens != None:
+            print(f"Tokens left: {tokenCount} | How many tokens do you want to take? (1-3)")
+            cpuNumber = rnd.randint(1, 3)
+            slp(1.5)
+            print(f"Computer chooses {cpuNumber} tokens.")
+            return tokenCount - cpuNumber
+        elif tokenCount <= 3 and tokenCount > 0 and tokens != None:
+            print(f"Tokens left: {tokenCount} | How many tokens do you want to take? (1-3)")
+            cpuNumber = tokenCount
+            slp(1.5)
+            print(f"Computer chooses {cpuNumber} tokens.")
+            return int(tokenCount - cpuNumber)
+        elif tokenCount == None:
+            pass
+
+def check_if_win(totalTokens):
+    if totalTokens == 0:
+        print("Big winner over here!")
+        return False
+    else:
+        print("Next turn comin' up!")
+        return True
+
 if __name__ == "__main__":
-    print(first_or_second())
+    turnOrder = first_or_second()
+    if turnOrder == "first" or turnOrder == "f":
+        while gameRun == True:
+            tokens = player_choose(tokens)
+            gameRun = check_if_win(tokens)
+            tokens = cpu_choose(tokens)
+            if tokens != None:
+                gameRun = check_if_win(tokens)
+    else:
+        print("CPU goes first")
