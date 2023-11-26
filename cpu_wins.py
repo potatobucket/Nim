0a7b229f595e56fbc8123b5943759e29c25dd83f
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-11-26 15:08:33 -0600

    CPU perfect play
    Made it so the computer always wins. At least going second. ;P
    
    (Might make it possible for it to win if it goes first)

--- cpu_wins.py
+++ cpu_wins.py
@@ -1,27 +1,20 @@
-import random as rnd
 from time import sleep as slp
 
 tokens = 24
 gameRun = True
-
-def first_or_second(turn = None):
-    validAnswers = ["first", "f", "last", "l"]
-    while turn not in validAnswers:
-        turn = input("Please choose whether you would like to go (F)IRST or (L)AST. ").lower()
-    return turn
 
 def player_choose(tokenCount):
     if tokens != 0:
         playerNumber = int(input(f"Tokens left: {tokenCount} | How many tokens do you want to take? (1-3) "))
         while playerNumber < 1 or playerNumber > 3:
             playerNumber = int(input(f"Tokens left: {tokenCount} | Please choose a number between 1 and 3. "))
-        return int(tokenCount - playerNumber)
+        return int(tokenCount - playerNumber), playerNumber
 
-def cpu_choose(tokenCount):
+def cpu_choose(tokenCount, playerChoice):
     if gameRun == True:
         if tokenCount > 3 and tokens != None:
             print(f"Tokens left: {tokenCount} | How many tokens do you want to take? (1-3)")
-            cpuNumber = rnd.randint(1, 3)
+            cpuNumber = 4 - playerChoice
             slp(1.5)
             print(f"Computer chooses {cpuNumber} tokens.")
             return tokenCount - cpuNumber
@@ -43,20 +36,10 @@ def check_if_win(totalTokens):
         return True
 
 if __name__ == "__main__":
-    turnOrder = first_or_second()
-    if turnOrder == "first" or turnOrder == "f":
-        print("Player goes first.")
-        while gameRun == True:
-            tokens = player_choose(tokens)
+    print("Player goes first.")
+    while gameRun == True:
+        tokens, playerTokenChoice = player_choose(tokens)
+        gameRun = check_if_win(tokens)
+        tokens = cpu_choose(tokens, playerTokenChoice)
+        if tokens != None:
             gameRun = check_if_win(tokens)
-            tokens = cpu_choose(tokens)
-            if tokens != None:
-                gameRun = check_if_win(tokens)
-    else:
-        print("CPU goes first.")
-        while gameRun == True:
-            tokens = cpu_choose(tokens)
-            gameRun = check_if_win(tokens)
-            tokens = player_choose(tokens)
-            if tokens != None:
-                gameRun = check_if_win(tokens)
